93cbdaafe3a098763ab40aa11abe0c9b4e9d8573
ca826 <55705@muni.cz>
2024-07-10 11:55:50 +0100

    Set up skeleton of contests app.

---
+++ contest/__init__.py
@@ -0,0 +1,56 @@
+
+from otree.api import *
+
+
+doc = """
+A Simple tullock contest
+Your app description
+"""
+
+
+class C(BaseConstants):
+    NAME_IN_URL = 'contest'
+    PLAYERS_PER_GROUP = None
+    NUM_ROUNDS = 1
+
+
+class Subsession(BaseSubsession):
+    pass
+
+
+class Group(BaseGroup):
+    pass
+
+
+class Player(BasePlayer):
+    pass
+
+
+# PAGES
+class Intro(Page):
+    pass
+
+
+class SetupRound(WaitPage):
+    pass
+
+
+class Decision(Page):
+    pass
+
+class WaitForDecisions(WaitPage):
+    pass
+
+class Results(Page):
+    pass
+
+class EndBlock(Page):
+    pass
+
+page_sequence = [Intro,
+                 SetupRound,
+                 Decision,
+                 WaitForDecisions,
+                 Results,
+                 EndBlock,
+]
